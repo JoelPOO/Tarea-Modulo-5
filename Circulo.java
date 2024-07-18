71135acc264866eb7f2601222572ba3eaacfc57a
JoelPOO <jo.herrera2590@gmail.com>
2024-07-18 16:08:32 -0600

    Add files via upload

---
+++ Circulo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Milton Sarmiento
+ */
+public class Circulo {
+  public class Circulo extends Formas {
+    public Circulo() {
+        super("Círculo");
+    }
+}  
+}
---
+++ Cuadrado.java
@@ -0,0 +1,20 @@
+
+import com.mycompany.formas.Formas;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Milton Sarmiento
+ */
+
+    public class Cuadrado extends Formas {
+    public Cuadrado() {
+        super("Cuadrado");
+    }
+}
+
+
